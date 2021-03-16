8c0d7fbc09c608e65f30ff4b878dc77dd726f497
Gijs <gijs@snapit.group>
2021-03-16 13:45:27 +1300

    grants updated

--- client/src/views/Grants.vue
+++ client/src/views/Grants.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="w-full overflow-x-hidden min-h-screen bg-black">
+  <div class="w-full overflow-x-hidden min-h-screen bgcolor">
     <div class="w-screen py-8 sm:py-24 bgpic">
-      <div class="w-10/12 grid grid-cols-1 sm:grid-cols-2 mx-auto items-center">
-        <img
-          class="w-1/2 pb-12 sm:pb-0 mx-auto sm:mx-0"
-          src="../assets/logo.png"
-        />
-        <h1 class="text-5xl text-white text-center pt-10">Greenhill Seltzer Grants</h1>
-        <!-- <button
-          class="justify-self-center sm:justify-self-end w-1/2 py-4 px-4 bg-transparent hover:bg-white text-white hover:text-black font-semibold border border-white rounded focus:outline-none"
-        >
-          Back to webiste
-        </button> -->
+      <div class="w-10/12 mx-auto grid grid-cols-1 sm:grid-cols-2 items-center align-center">
+        <div class="text-center sm:text-left">
+          <img class="w-3/4 sm:w-1/2 pb-8 mx-auto sm:mx-0" src="../assets/logo.png" />
+          <button
+            class="w-1/2 mb-8 sm:mb-0 sm:py-4 px-4 bg-transparent hover:bg-white text-white hover:text-black font-semibold border border-white rounded focus:outline-none"
+          >
+            Back to webiste
+          </button>
+        </div>
+        <h1 class="text-4xl lg:text-5xl text-white text-center sm:justify-self-end">
+          Greenhill Seltzers Grants
+        </h1>
       </div>
     </div>
     <div class="w-10/12 mx-auto py-12 text-white">
@@ -54,9 +55,11 @@
             culpa qui officia deserunt mollit anim id est laborum."
           </p>
         </div>
-        <img src="../assets/bg.jpg" class="rounded w-3/5 sm:1/2 mx-auto my-auto" />
+        <img
+          src="../assets/bg.jpg"
+          class="rounded w-3/5 sm:1/2 mx-auto my-auto"
+        />
       </div>
-
 
       <div class="text-white text-right py-10">
         <h1 class="font-bold text-4xl pb-4">How to Apply?</h1>
@@ -93,5 +96,9 @@
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
+}
+
+.bgcolor {
+  background-color: #1a1a1a;
 }
 </style>
